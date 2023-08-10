f356eceec73fdb4ed0a3d9e4d4f11e72e77d8c0b
Oussama Sethoum <os.sethoum@gmail.com>
2023-08-10 11:26:40 +0100

    fix name error

--- server/src/routes/auth/auth.validator.ts
+++ server/src/routes/auth/auth.validator.ts
@@ -6,7 +6,7 @@ export const loginBodySchema = object({
 });
 
 export const registerBodySchema = object({
-  name: string([regex(/^w+( w+)*$/, "invalid email")]),
+  name: string([regex(/^(?!\\d+$)\\w+(?: \\w+)*$/, "invalid name")]),
   email: string([email("invalid email")]),
   password: string([minLength(6, "password too short")]),
 });
